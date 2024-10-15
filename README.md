559e65de49ed91a69698f9a65083496c92de029e
Teddy K <teddykoerniadi@gmail.com>
2024-10-15 13:19:26 +0700

    Update .gitignore, enhance README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# fork-script
+# Fork Script
+
+## Pre-requisites
+- (Github CLI)[https://cli.github.com/]
+
+## Usage
+1. Clone the repository
+2. Copy `.env.example` to `.env` and fill in the required values
+3. Copy `students.txt.example` to `students.txt` and fill in the required values
+4. Give execute permission to the script `chmod +x fork-assignment.sh`
+5. Run the script `./fork-assignment.sh`
+6. See the output folder
--- fork-assisgnment.sh
+++ fork-assisgnment.sh
@@ -11,8 +11,10 @@ echo "=========================="
 
 TEMPLATE="$ORGANIZATION/$REPO"
 
+output='['
+
 for STUDENT in $(cat students.txt); do
-  echo "Processing $STUDENT..."
+  echo "-> Processing $STUDENT..."
 
   gh repo fork "$TEMPLATE" --clone=false --remote=false --org="$ORGANIZATION" --fork-name="$REPO-$STUDENT"
 
@@ -26,8 +28,20 @@ for STUDENT in $(cat students.txt); do
         "/repos/$STUDENT_REPO/collaborators/$STUDENT" \
         -f permission=push > /dev/null 2>&1
 
-  echo "Completed setup for $STUDENT."
+  output+='{"username": "'$STUDENT'", "repo": "'http://github.com/$STUDENT_REPO'"}, '
+
+
+  echo "-> Completed setup for $STUDENT."
 done
+
+output=${output%,*}   
+output+=']'           
+
+if [ ! -d output ]; then
+  mkdir output
+fi
+
+echo "$output" > ./output/$REPO.json
 
 echo "=========================="
 echo "Forking Assignment Completed"
